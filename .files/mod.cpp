01b0153c7f601f60e770ea4d71c58a9505dc4872
ir0n1e <ir0n1e@freenet.de>
2014-11-08 06:48:53 +0100

    added logos and mod.cpp

---
+++ .files/mod.cpp
@@ -0,0 +1,5 @@
+name = "BW.plus";
+picture = "BWplus_logo.paa";
+actionName = "Website";
+action = "https://github.com/ir0n1e/BWplus";
+description = "BW.plus - Additional skins and features for BWmod and AGM";
