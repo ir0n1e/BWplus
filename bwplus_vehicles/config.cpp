c95f82502f59b627a6ad851213b4c85b7fb56a7c
ir0n1e <ir0n1e@freenet.de>
2015-03-07 15:16:04 +0100

    removed old paa's

--- bwplus_vehicles/config.cpp
+++ bwplus_vehicles/config.cpp
@@ -596,3 +596,19 @@ class CfgVehicles {
     	};
     };
 };
+
+enum {
+	destructengine = 2,
+	destructdefault = 6,
+	destructwreck = 7,
+	destructtree = 3,
+	destructtent = 4,
+	stabilizedinaxisx = 1,
+	stabilizedinaxesxyz = 4,
+	stabilizedinaxisy = 2,
+	destructno = 0,
+	stabilizedinaxesboth = 3,
+	stabilizedinaxesnone = 0,
+	destructman = 5,
+	destructbuilding = 1
+}
